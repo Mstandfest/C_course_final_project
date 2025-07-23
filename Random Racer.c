6502d04a61a7693c61d642e7ecbc8c18ee9083f3
Mstandfest <m-standfest@web.de>
2025-07-23 21:18:02 +0200

    Add files via upload

--- Random Racer.c
+++ Random Racer.c
@@ -11,22 +11,18 @@ struct Race {
   char firstPlaceDriverName[50];
   char firstPlaceRaceCarColor[20];
 };
-// Eine für die Fahrzeuge mit dem Fahrernamen, der Farbe und den Variablen die wir später für die logischen Funktionen benötigen:
-// Gesamtzeit, Geschwindigkeit (1-3), Beschleunigung (1-3) und Nerven (1-3)
+// Eine für die Fahrzeuge mit dem Fahrernamen, der Farbe und der Gesamtzeit, die wir später für die logischen Funktionen benötigen:
 struct RaceCar {
   char driverName[50];
   char raceCarColor[20];
   int totalLapTime;
-  int speed;
-  int acceleration;
-  int nerves;
 };
 // Funktionen für die Ausgabe
 // Einleitung zum Rennen
 void printIntro() {
   printf("Herzlich Willkommen zu unserem heutigen digitalen Rennen, liebe Freunde des Rennsports!\n");
   printf("Holt euch euer Popcorn und macht euch bereit!\n");
-};
+}
 // Countdown vor dem Rennen von 5-0
 void printCountDown() {
   printf("Fahrer bereit machen!\n");
@@ -36,34 +32,33 @@ void printCountDown() {
   printf("2\n");
   printf("1\n");
   printf("LOS!!!\n");
-};
+}
 // Ausgabe des aktuell führenden nach jeder Runde
 void printFirstPlaceAfterLap(struct Race race) {
   printf("Nach runde %d führt:\n", race.currentLap);
   printf("%s im %s Fahrzeug!\n", race.firstPlaceDriverName, race.firstPlaceRaceCarColor);
-};
+}
 // Ausgabe des Siegers
 void printCongratulation(struct Race race) {
   printf("Herzlichen Glückwunsch an %s im %s Fahrzeug für diese großartige Leistung!\n", race.firstPlaceDriverName, race.firstPlaceRaceCarColor);
   printf("Es war ein beeindruckendes Rennen! Wir wünschen allen Zuschauern eine gute Nacht!\n");
-};
+}
 // Logische Funktionen
 // Berechnung der Zeit für eine Runde
 /* Hier werden 3 int Varialen randomisiert (zwischen 1 und 3) vergeben:
    speed, acceleration, nerves
    Die Summe der Werte ergeben die gesamtpunktzahl und somit die Geschwindigkeit */
-int calculateTimeToCompleteLap(int speed, int acceleration, int nerves) {
-  speed = rand() % (3 - 1 ) + 1;
-  acceleration = rand() % (3 - 1) + 1;
-  nerves = rand() % (3 - 1) + 1;
-  int sum = speed + acceleration + nerves;
-  return sum;
-};
+int calculateTimeToCompleteLap() {
+  int speed = rand() % 3 + 1;
+  int acceleration = rand() % 3 + 1;
+  int nerves = rand() % 3 + 1;
+  return speed + acceleration + nerves;
+}
 // Berechnung der Gesamtzeit der Fahrzeuge
 void updateRaceCar(struct RaceCar* raceCar){
-  int lapTime = calculateTimeToCompleteLap(raceCar->speed, raceCar->acceleration, raceCar->nerves);
+  int lapTime = calculateTimeToCompleteLap(raceCar);
   raceCar->totalLapTime += lapTime;
-};
+}
 // Funktion um den ersten Platz zu aktualisieren
 void updateFirstPlace(struct Race* race, struct RaceCar* raceCar1, struct RaceCar* raceCar2) {
   // if-Funktion zur Überprüfung welches Fahrzeug die schnellere Gesamtzeit besitzt
@@ -76,7 +71,7 @@ void updateFirstPlace(struct Race* race, struct RaceCar* raceCar1, struct RaceCa
     strcpy(race->firstPlaceDriverName, raceCar2->driverName);
     strcpy(race->firstPlaceRaceCarColor, raceCar2->raceCarColor);
   }
-};
+}
 // In dieser Funktion wird das Rennen gestartet und die Rundenzahlen eingestellt.
 void startRace(struct RaceCar* raceCar1, struct RaceCar* raceCar2) {
   struct Race race = {5, 1, "", ""};
@@ -89,21 +84,21 @@ void startRace(struct RaceCar* raceCar1, struct RaceCar* raceCar2) {
     printFirstPlaceAfterLap(race);
     // Hochzählen der Rundenzahlen, vor allem für die FirstPlaceAfterLap-Funktion
     race.currentLap++;
-  };
+  }
   // Ausgabe des Gewinners, nachdem die for-Schleife 5 mal durchgelaufen und das Rennen
   // somit beendet ist
   printCongratulation(race);
-};
+}
 // Hauptfunktion:
 int main() {
 	// Definieren der beiden Teilnehmer mit Name und Fahrzeugfarbe
   srand(time(0));
-  struct RaceCar raceCar1 = {"Michael Schumacher", "roten"};
-  struct RaceCar raceCar2 = {"Mika Häkkinen", "silbernen"};
+  struct RaceCar raceCar1 = {"Michael Schumacher", "roten", 0};
+  struct RaceCar raceCar2 = {"Mika Häkkinen", "silbernen", 0};
   // Ausgabe des Willkommenstextes
   printIntro();
   // Countdown für den Start
   printCountDown();
   // Ausführen des Rennens
   startRace(&raceCar1, &raceCar2);
-};
+}
